be94fe866b8d16fb27b215cca76e1598f37b1a87
wilderidge <1104542252.@qq.com>
2024-06-12 19:53:20 +0800

    1

--- lab1.py
+++ lab1.py
@@ -147,14 +147,24 @@ def fun_5(output_path):
 
 
 def calc_shortest_path(G, word1, word2):
-    shortest_path = nx.shortest_path(G, source=word1, target=word2, weight='weight')
+    # 检查 word1 和 word2 是否为空字符串
+    if word1 == "" or word2 == "":
+        return [ ], 0
+    # 将 word1 和 word2 都转换为小写
+    word1 = word1.lower()
+    word2 = word2.lower()
+    # 检查 word1 和 word2 是否都在图中
+    if word1 not in G or word2 not in G:
+        return [ ], 0
+    try:
+        shortest_path = nx.shortest_path(G, source=word1, target=word2, weight='weight')
+    except nx.NetworkXNoPath:
+        return [ ], 0
     path_edges = [(shortest_path[i], shortest_path[i + 1]) for i in range(len(shortest_path) - 1)]
     path_length = 0
     for u, v in path_edges:
         edge_data = G.get_edge_data(u, v)
         min_weight = min(data['weight'] for data in edge_data.values())
-
-        print(f"Edge {u} -> {v} has weight {min_weight}")
         path_length += min_weight
 
     return path_edges, path_length
