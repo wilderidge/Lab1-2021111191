36730e24d89b2413a57b799195671c950585bdbb
wilderidge <1104542252.@qq.com>
2024-06-12 19:40:09 +0800

    lab3b

--- lab1.py
+++ lab1.py
@@ -54,7 +54,7 @@ def generate_directed_graph(input_path):
             G.remove_edge(*edge)
             # Increase the count
             edge_counts[edge] += 1
-            print(f"Edge {edge} has been added {edge_counts[edge]} times.")
+            # print(f"Edge {edge} has been added {edge_counts[edge]} times.")
         else:
             edge_counts[edge] = 1
 
@@ -62,7 +62,7 @@ def generate_directed_graph(input_path):
         G.add_edge(*edge, key=edge_counts[edge], weight=edge_counts[edge])
 
     # 设置随机种子
-    np.random.seed(0)
+    np.random.seed(1234)
     pos = nx.spring_layout(G)
     return G, pos, words
 
@@ -147,14 +147,24 @@ def fun_5(output_path):
 
 
 def calc_shortest_path(G, word1, word2):
-    shortest_path = nx.shortest_path(G, source=word1, target=word2, weight='weight')
+    # 检查 word1 和 word2 是否为空字符串
+    if word1 == "" or word2 == "":
+        return [ ], 0
+    # 将 word1 和 word2 都转换为小写
+    word1 = word1.lower()
+    word2 = word2.lower()
+    # 检查 word1 和 word2 是否都在图中
+    if word1 not in G or word2 not in G:
+        return [ ], 0
+    try:
+        shortest_path = nx.shortest_path(G, source=word1, target=word2, weight='weight')
+    except nx.NetworkXNoPath:
+        return [ ], 0
     path_edges = [(shortest_path[i], shortest_path[i + 1]) for i in range(len(shortest_path) - 1)]
     path_length = 0
     for u, v in path_edges:
         edge_data = G.get_edge_data(u, v)
         min_weight = min(data['weight'] for data in edge_data.values())
-
-        print(f"Edge {u} -> {v} has weight {min_weight}")
         path_length += min_weight
 
     return path_edges, path_length
@@ -224,3 +234,10 @@ def randomWalk():
         time.sleep(1)
 
     return st.session_state.text
+
+if __name__ == '__main__':
+    chong_zhi()
+    fun_3()
+    generate_new_text('input/2.txt')
+    fun_5('output/2.png')
+    randomWalk()
---
+++ test.py
@@ -0,0 +1,37 @@
+from lab1 import *
+
+def test_shortest_path_1():
+    G, pos, words = generate_directed_graph('input/1.txt')
+    path_edges, path_length = calc_shortest_path(G, 'to', 'and')
+    assert path_edges == [('to', 'explore'), ('explore', 'strange'), ('strange', 'new'), ('new', 'life'), ('life', 'and')], path_edges
+    assert path_length == 20, path_length
+
+def test_shortest_path_2():
+    G, pos, words = generate_directed_graph('input/1.txt')
+    path_edges, path_length = calc_shortest_path(G, 'To', 'AND')
+    assert path_edges == [('to', 'explore'), ('explore', 'strange'), ('strange', 'new'), ('new', 'life'), ('life', 'and')], path_edges
+    assert path_length == 20, path_length
+
+def test_shortest_path_3():
+    G, pos, words = generate_directed_graph('input/1.txt')
+    path_edges, path_length = calc_shortest_path(G, 'AnD', 'To')
+    assert path_edges == [('and', 'new'), ('new', 'worlds'), ('worlds', 'to')], path_edges
+    assert path_length == 12, path_length
+
+def test_shortest_path_4():
+    G, pos, words = generate_directed_graph('input/1.txt')
+    path_edges, path_length = calc_shortest_path(G, 'a', 'c')
+    assert path_edges == [ ], path_edges
+    assert path_length == 0, path_length
+
+def test_shortest_path_5():
+    G, pos, words = generate_directed_graph('input/1.txt')
+    path_edges, path_length = calc_shortest_path(G, 'and', '')
+    assert path_edges == [ ], path_edges
+    assert path_length == 0, path_length
+
+def test_shortest_path_6():
+    G, pos, words = generate_directed_graph('input/1.txt')
+    path_edges, path_length = calc_shortest_path(G, 'and', 'ww')
+    assert path_edges == [ ], path_edges
+    assert path_length == 0, path_length
